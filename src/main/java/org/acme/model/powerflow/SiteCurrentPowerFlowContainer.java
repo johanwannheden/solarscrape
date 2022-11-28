c2b9d75793dbb5d7706f49611695c87869104ef6
Johan Wannheden <johan.wannheden@gmail.com>
2022-11-28 21:15:51 +0100

    convert pojo classes to records

--- src/main/java/org/acme/model/powerflow/SiteCurrentPowerFlowContainer.java
+++ src/main/java/org/acme/model/powerflow/SiteCurrentPowerFlowContainer.java
@@ -1,38 +1,4 @@
 package org.acme.model.powerflow;
 
-import java.util.Objects;
-
-public final class SiteCurrentPowerFlowContainer {
-    private final SiteCurrentPowerFlow siteCurrentPowerFlow;
-
-    public SiteCurrentPowerFlowContainer(SiteCurrentPowerFlow siteCurrentPowerFlow) {
-        this.siteCurrentPowerFlow = siteCurrentPowerFlow;
-    }
-
-    public SiteCurrentPowerFlow siteCurrentPowerFlow() {
-        return siteCurrentPowerFlow;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null || obj.getClass() != this.getClass()) {
-            return false;
-        }
-        var that = (SiteCurrentPowerFlowContainer) obj;
-        return Objects.equals(this.siteCurrentPowerFlow, that.siteCurrentPowerFlow);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(siteCurrentPowerFlow);
-    }
-
-    @Override
-    public String toString() {
-        return "SiteCurrentPowerFlowContainer[" + "siteCurrentPowerFlow=" + siteCurrentPowerFlow + ']';
-    }
-
+public record SiteCurrentPowerFlowContainer(SiteCurrentPowerFlow siteCurrentPowerFlow) {
 }
